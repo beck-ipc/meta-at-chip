d8e13f8a95b0c6784a3d5a2e9d1a1b3017ac8633
Florian Boor <florian.boor@kernelconcepts.de>
2017-07-03 11:53:40 +0200

    application example: Set up some GPIO SysFS interfaces.

--- recipes-examples/application/files/StartApplication.sh
+++ recipes-examples/application/files/StartApplication.sh
@@ -4,7 +4,26 @@ echo "Starting application..."
 # enable backlight
 
 i2cset -y 3 0x17 0x00 0xfc
- 
+
+# export gpio
+
+echo 496 > /sys/class/gpio/export
+echo 497 > /sys/class/gpio/export
+echo 498 > /sys/class/gpio/export
+echo 499 > /sys/class/gpio/export
+echo 500 > /sys/class/gpio/export
+echo 501 > /sys/class/gpio/export
+echo 502 > /sys/class/gpio/export
+echo 503 > /sys/class/gpio/export
+
+echo out > /sys/class/gpio/gpio496/direction
+echo out > /sys/class/gpio/gpio497/direction
+echo out > /sys/class/gpio/gpio498/direction
+echo out > /sys/class/gpio/gpio499/direction
+echo out > /sys/class/gpio/gpio500/direction
+echo out > /sys/class/gpio/gpio501/direction
+echo out > /sys/class/gpio/gpio502/direction
+echo out > /sys/class/gpio/gpio503/direction
 
 # run application
 
